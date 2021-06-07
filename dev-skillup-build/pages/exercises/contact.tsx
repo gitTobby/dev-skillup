014b1ec6ee69a71ce9563874befa2f5e6f0c4447
iTobby <coketack@gmail.com>
2021-06-07 15:06:06 +0900

    fixed

--- dev-skillup-build/pages/exercises/contact.tsx
+++ dev-skillup-build/pages/exercises/contact.tsx
@@ -3,3 +3,40 @@ const Contact = () => {
 };
 
 export default Contact;
+
+// import React, { useState } from 'react';
+// import SearchBox from './SearchBox';
+// import ContactList from './ContactList';
+// import Info from './Info';
+// import Empty from './Empty';
+
+// const Contact = () => {
+//   const [keyword, setKeyword] = useState<string>('');
+//   const [selected, setSelected] = useState<boolean>();
+
+//   const changeKeyword = (keyword: any) => {
+//     setKeyword(keyword);
+//     setSelected('');
+//   };
+
+//   const selectList = (info: any) => {
+//     setSelected(info);
+//   };
+
+//   return (
+//     <div className="container ex-contact">
+//       <h1 className="subject">김동혁의 연락처</h1>
+//       <div className="contact-wrap">
+//         <div className="col left">
+//           <SearchBox keyword={keyword} changeKeyword={changeKeyword} />
+//           <ContactList keyword={keyword} selected={selected} selectList={selectList} />
+//         </div>
+//         <div className="col right">
+//           <div className="details">{selected ? <Info keyword={keyword} selected={selected} /> : <Empty />}</div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Contact;
